4944c0c3a4cf9eb267d6b785cb67f12457062bd6
Jakob Meng <jakobmeng@web.de>
2019-10-08 14:30:57 +0200

    Added notes about eig() implementations in Elemental and MATLAB

--- src/hbrs/mpl/detail/matlab_cxn/impl/eig_level0.m
+++ src/hbrs/mpl/detail/matlab_cxn/impl/eig_level0.m
@@ -5,6 +5,21 @@
 % References:
 %  https://de.mathworks.com/help/matlab/ref/eig.html
 
+% NOTE:
+% Native MATLAB may use different algorithms for eig() than MATLAB Coder.
+% If input matrix is not Hermitian, then MATLAB Coder uses the QZ algorithm.
+% The results from "[V,D] = eig(A)" in MATLAB Coder will be similar to
+% "[V,D] = eig(A,eye(size(A)),'qz')", except that MATLAB Coder normalizes the
+% columns of V. Consequently, V may represent a different basis of eigenvectors,
+% and the eigenvalues in D may not be in the same order as in MATLAB.
+%
+% For eig(A) and if A is not Hermitian, MATLAB Coder uses LAPACK's xGEEV.
+% For eig(A,B), MATLAB Coder uses LAPACK's xGGEV.
+%
+% Ref.:
+%  R2018b/toolbox/eml/lib/matlab/matfun/eig.m
+%  https://stackoverflow.com/a/26810521/6490710
+
 function [eigenvalues, eigenvectors] = eig_level0(A)
     coder.varsize('A', 'x');
     
--- src/hbrs/mpl/fn/eig/impl/elemental.hpp
+++ src/hbrs/mpl/fn/eig/impl/elemental.hpp
@@ -38,6 +38,52 @@ HBRS_MPL_NAMESPACE_BEGIN
 namespace hana = boost::hana;
 namespace detail {
 
+/* Code of El::Eig() [1] is similar to:
+ * ( Matrix<Complex<Real>>& A,
+ *   Matrix<Complex<Real>>& w,
+ *   Matrix<Complex<Real>>& X )
+ * {
+ *     // A is input matrix
+ *     // w contains eigenvalues of A
+ *     // X contains eigenvectors of A.
+ *     typedef Complex<Real> C;
+ * 
+ *     Schur( A, w, X );
+ * 
+ *     Matrix<C> R;
+ *     TriangEig( A, R );
+ * 
+ *     Trmm( RIGHT, UPPER, NORMAL, NON_UNIT, C(1), R, X );
+ * }
+ * 
+ * Schur() computes the Schur decomposition, using LAPACK's Hessenberg QR
+ * algorithm (with Aggressive Early Deflation) for local matrices, and if
+ * support for ScaLAPACK was detected during configuration, ScaLAPACK's
+ * Hessenberg QR algorithm (without deflation) [2].
+ * 
+ * TriangEig() computes eigenvectors of a triangular matrix [3] 
+ * and is implemented in [4].
+ * 
+ * xTRMM() from BLAS performs " :math:`C := \alpha \mbox{op}(A) B` or
+ * :math:`C := \alpha B \mbox{op}(A)`, depending upon whether `side` was 
+ * chosen as 'L' or 'R', respectively. Whether :math:`A` is treated as lower 
+ * or upper triangular is determined by whether `uplo` is 'L' or 'U' (setting
+ * `unit` equal to 'U' treats :math:`A` as unit diagonal, otherwise it should
+ * be set to 'N'). :math:`\mbox{op}` is determined in the same manner as in 
+ * ``blas::Trmv``." [5]
+ * 
+ * NOTE: MATLAB Coder uses LAPACK's xGEEV for eig() and thus
+ *       will likely produce different decompositions [6].
+ * 
+ * Ref.:
+ * [1] Elemental/src/lapack_like/spectral/Eig.cpp 
+ * [2] elemental-web/source/doc-dev/lapack_like/spectral/Schur.rst
+ * [3] Elemental/include/El/lapack_like/spectral.hpp
+ * [4] Elemental/src/lapack_like/spectral/TriangEig.cpp
+ * [5] elemental-web/source/doc-dev/core/imports/blas.rst
+ * [6] src/hbrs/mpl/detail/matlab_cxn/impl/eig_level0.m
+ */
+
 template <
 	typename Ring,
 	typename std::enable_if_t<
