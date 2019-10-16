472230ba15069e893e14ab3c7776f4145e2ed7fc
Jakob Meng <jakobmeng@web.de>
2019-10-16 09:39:34 +0200

    Increased no. of inner iterations in Elemental's SVD and clarify shortcomings of ScaLAPACK's PDGESVD
    Depending on the numerical properties of the input matrix, ScaLAPACK's PDGESVD fails with INFO = MIN(M,N) + 1
    and Elemental then throws a "std::runtime_error: pdgesvd exited with info=..". INFO > 0 means that DBDSQR did not
    converge a.k.a. PDGESVD has detected heterogeneity by finding that eigenvalues were not identical across the
    process grid. In this case, the accuracy of the results from PDGESVD cannot be guaranteed [1].
    
    According to Demmel and Kahan (1990) algorithm convergence only depends on the matrix elements and values
    computed by the DBDSQR algorithm, but not on the size or shape of the input matrix [3].
    
    ScaLAPACK's DBDSQR has a internal variable MAXITR, that controls the maximum number of passes of the algorithm
    through its inner loop. The algorithms stops (and so fails to converge) if the number of passes through the
    inner loop exceeds MAXITR*N**2 [2].
    
    MAXITR cannot be changed except though changing and recompiling DBDSQR's Fortran code so we use Elemental's SVD,
    which allows changing this value through El::SVDCtrl<>::bidiagSVDCtrl.qrCtrl.maxIterPerVal variable.
    
    Ref.:
    [1] http://www.netlib.org/scalapack/explore-html/d2/de6/pdgesvd_8f_source.html
    [2] http://www.netlib.org/lapack/lapack-3.1.1/html/dbdsqr.f.html
    [3] http://www.netlib.org/lapack/lawnspdf/lawn03.pdf

--- src/hbrs/mpl/fn/svd/impl/elemental.hpp
+++ src/hbrs/mpl/fn/svd/impl/elemental.hpp
@@ -42,14 +42,32 @@ svd_impl_el(A const& a, svd_control<decompose_mode> const& ctrl, U u, S s, S s_,
 	El::SVDCtrl<El::Base<_Ring_>> el_ctrl;
 	
 	el_ctrl.useLAPACK = true;
-	/* Disabled ScaLAPACK because if number of MPI processes is larger than maximum(m(size(a)), n(size(a))),
-	 * then Elemental throws a "std::runtime_error: pdgesvd exited with info=..". More precisely, PDGESVD
-	 * fails with INFO = MIN(M,N) + 1 if number of MPI processes is larger than MAX(M,N).
-	 * TODO: Solve this bug!
+	/* Use Elemental's native SVD instead of ScaLAPACK's PDGESVD.
+	 * 
+	 * Depending on the numerical properties of the input matrix, ScaLAPACK's PDGESVD fails with INFO = MIN(M,N) + 1
+	 * and Elemental then throws a "std::runtime_error: pdgesvd exited with info=..". INFO > 0 means that DBDSQR did not
+	 * converge a.k.a. PDGESVD has detected heterogeneity by finding that eigenvalues were not identical across the
+	 * process grid. In this case, the accuracy of the results from PDGESVD cannot be guaranteed [1].
+	 * 
+	 * According to Demmel and Kahan (1990) algorithm convergence only depends on the matrix elements and values
+	 * computed by the DBDSQR algorithm, but not on the size or shape of the input matrix [3].
+	 * 
+	 * ScaLAPACK's DBDSQR has a internal variable MAXITR, that controls the maximum number of passes of the algorithm
+	 * through its inner loop. The algorithms stops (and so fails to converge) if the number of passes through the 
+	 * inner loop exceeds MAXITR*N**2 [2].
+	 * 
+	 * MAXITR cannot be changed except though changing and recompiling DBDSQR's Fortran code so we use Elemental's SVD,
+	 * which allows changing this value through El::SVDCtrl<>::bidiagSVDCtrl.qrCtrl.maxIterPerVal variable.
+	 * 
+	 * Ref.: 
+	 * [1] http://www.netlib.org/scalapack/explore-html/d2/de6/pdgesvd_8f_source.html
+	 * [2] http://www.netlib.org/lapack/lapack-3.1.1/html/dbdsqr.f.html
+	 * [3] http://www.netlib.org/lapack/lawnspdf/lawn03.pdf
 	 */
 	el_ctrl.useScaLAPACK = false;
 	el_ctrl.bidiagSVDCtrl.wantU = true;
 	el_ctrl.bidiagSVDCtrl.wantV = true;
+	el_ctrl.bidiagSVDCtrl.qrCtrl.maxIterPerVal *= 2; // default value is 6
 	
 	if (ctrl.decompose_mode() == decompose_mode::complete) {
 		el_ctrl.bidiagSVDCtrl.approach = El::SVDApproach::FULL_SVD;
